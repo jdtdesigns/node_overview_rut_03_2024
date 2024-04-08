14a6ec5bf20fd7e1092ad64d1fe722c7c5af3db7
JD Tadlock <jt7903@gmail.com>
2024-04-07 20:15:12 -0400

    completed inquirer student app example

--- index.js
+++ index.js
@@ -1,29 +1,98 @@
 const fs = require('fs')
-const studentName = process.argv[2]
+const inquirer = require('inquirer')
 
-if (studentName === 'print') {
-  fs.readFile('./students.txt', 'utf8', (err, data) => {
-    if (err) {
-      return console.log(err)
+function getNames() {
+  const data = fs.readFileSync('./students.txt', 'utf8')
+
+  return data.split('\n')
+}
+
+
+function showMenu() {
+  inquirer.prompt({
+    message: 'Choose a menu option',
+    type: 'list',
+    choices: ['List Students', 'Get a random student', 'Add a student', 'Exit'],
+    name: 'choice'
+  }).then((answerObj) => {
+    if (answerObj.choice === 'List Students') {
+      const names = getNames()
+
+      console.log('\nStudent List:', '\n-------------')
+
+      names.forEach((name) => {
+        console.log(name)
+      })
+
+      showMenu()
+    } else if (answerObj.choice === 'Get a random student') {
+      // Get student names and convert to array
+      const names = getNames()
+
+      // Grab random name from students array
+      const randomIndex = Math.floor(Math.random() * names.length)
+      const randomStudent = names[randomIndex]
+
+      // Print random name to terminal
+      console.log(randomStudent, 'has been chosen!')
+      showMenu()
+    } else if (answerObj.choice === 'Add a student') {
+      inquirer.prompt({
+        message: 'Type the student name',
+        name: 'name',
+      }).then((answerObj) => {
+        fs.appendFile('./students.txt', '\n' + answerObj.name, (err) => {
+          if (err) {
+            return console.log(err)
+          }
+
+          console.log('Student has been added!')
+        })
+
+        showMenu()
+      })
+    } else {
+      console.log('Thanks for using our student app!')
     }
-
-    const names = data.split('\n')
-
-    console.log('\nStudent List:', '\n', '----------')
-
-    for (let name of names) {
-      console.log(name)
-    }
-  })
-} else {
-  fs.appendFile('./students.txt', '\n' + studentName, (err) => {
-    if (err) {
-      return console.log(err)
-    }
-
-    console.log('Student has been added!')
   })
 }
+
+showMenu()
+
+
+// function getNames() {
+//   const data = fs.readFileSync('./students.txt', 'utf8')
+
+//   return data.split('\n')
+// }
+
+// if (studentName === 'print') {
+//   const names = getNames()
+
+//   console.log('\nStudent List:', '\n-------------')
+
+//   names.forEach((name) => {
+//     console.log(name)
+//   })
+// } else if (studentName === 'random') {
+//   // Get student names and convert to array
+//   const names = getNames()
+
+//   // Grab random name from students array
+//   const randomIndex = Math.floor(Math.random() * names.length)
+//   const randomStudent = names[randomIndex]
+
+//   // Print random name to terminal
+//   console.log(randomStudent, 'has been chosen!')
+// } else {
+//   fs.appendFile('./students.txt', '\n' + studentName, (err) => {
+//     if (err) {
+//       return console.log(err)
+//     }
+
+//     console.log('Student has been added!')
+//   })
+// }
 
 
 
