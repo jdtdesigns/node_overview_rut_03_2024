8144b02d991e5c150d16fd2969b5e595d8b16235
JD Tadlock <jt7903@gmail.com>
2024-04-08 15:00:06 -0400

    finished basic inquirer intro

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const { print, add } = require('./lib/studentTools.js')
-const dayjs = require('dayjs')
+const inquirer = require('inquirer')
+const save = require('./lib/save')
 
-const studentName = process.argv[2]
+inquirer.prompt([
+  {
+    name: 'age',
+    message: 'Please type your age'
+  },
+  {
+    name: 'full_name',
+    message: 'Please type your full name'
+  },
+  {
+    name: 'position',
+    type: 'list',
+    choices: ['Manager', 'Clerk', 'Finance']
+  },
+  {
+    name: 'fav',
+    type: 'checkbox',
+    message: 'Please choose your favorite food type',
+    choices: ['Korean', 'Southern', 'Turkish']
+  }
+]).then(save)
 
-if (studentName === 'print') {
-  print()
-} else {
-  const date = dayjs().format('MM/DD/YYYY')
-
-  add(studentName + ' - ' + date)
-}
